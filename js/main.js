898aae36eb59f6d4d8305dd24e6af35bfe9db6b2
andlit0vv <andlit0vv@gmail.com>
2026-02-19 00:19:10 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,3 +11,14 @@ function animate() {
 }
 
 animate();
+
+// Header shadow on scroll
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 30) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
